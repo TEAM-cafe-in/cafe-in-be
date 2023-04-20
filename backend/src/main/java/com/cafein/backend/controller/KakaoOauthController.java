819ffd9a8f2cfcf29d6ae556d4272bca3fc5afe3
Seph-23 <seongmlyu95@gmail.com>
2023-04-20 16:21:36 +0900

    feat: Kakao Oauth2.0 Login을 구현한다 #1
    Co-authored-by: euichanhwang <pine6971@naver.com>

--- backend/src/main/java/com/cafein/backend/controller/KakaoOauthController.java
+++ backend/src/main/java/com/cafein/backend/controller/KakaoOauthController.java
@@ -1,7 +1,10 @@
 package com.cafein.backend.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,8 +21,10 @@ public class KakaoOauthController {
 
 	private final KakaoOathService kakaoOathService;
 
+	@GetMapping("/login/{provider}")
 	public ResponseEntity<LoginResponseDTO> login(@PathVariable String provider, @RequestParam String code) {
 		final LoginResponseDTO loginResponseDto = kakaoOathService.login(provider, code);
+		System.out.println(provider);
 		return ResponseEntity.ok().body(loginResponseDto);
 	}
 }
