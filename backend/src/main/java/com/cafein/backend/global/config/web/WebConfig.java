282e8dbc9f4d5749317b1580ee184e828062d878
유성민 <97558688+Seph-23@users.noreply.github.com>
2023-05-04 14:50:28 +0900

    feat: XSS 공격에 대한 보안을 강화한다. #30 (#31)
    build: build.gradle에 naver lucy-xss 와 apache:commons-text 추가.
    HtmlCharacterEscapes 클래스 구현.
    WebConfig에 FilterRegistrationBean, configuremessageConverters, MappingJackson2HttpMessageConverter 추가.
    XssTest Controller 와 DTO 생성.
    
    Co-authored-by: euichanhwang <pine6971@naver.com>

--- backend/src/main/java/com/cafein/backend/global/config/web/WebConfig.java
+++ backend/src/main/java/com/cafein/backend/global/config/web/WebConfig.java
@@ -1,20 +1,31 @@
 package com.cafein.backend.global.config.web;
 
+import java.util.List;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.cafein.backend.global.interceptor.AuthenticationInterceptor;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.RequiredArgsConstructor;
+
+import com.navercorp.lucy.security.xss.servletfilter.XssEscapeServletFilter;
 
 @Configuration
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
 	private final AuthenticationInterceptor authenticationInterceptor;
+	private final ObjectMapper objectMapper;
 
 	@Override
 	public void addCorsMappings(final CorsRegistry registry) {
@@ -39,5 +50,26 @@ public class WebConfig implements WebMvcConfigurer {
 			.excludePathPatterns("/api/oauth/login",
 				"/api/access-token/issue",
 				"/api/logout");
+	}
+
+	@Bean
+	public FilterRegistrationBean<XssEscapeServletFilter> filterRegistrationBean() {
+		FilterRegistrationBean<XssEscapeServletFilter> filterRegistration = new FilterRegistrationBean<>();
+		filterRegistration.setFilter(new XssEscapeServletFilter());
+		filterRegistration.setOrder(1);
+		filterRegistration.addUrlPatterns("/*");
+		return filterRegistration;
+	}
+
+	@Override
+	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+		converters.add(jsonEscapeConverter());
+	}
+
+	@Bean
+	public MappingJackson2HttpMessageConverter jsonEscapeConverter() {
+		ObjectMapper copy = this.objectMapper.copy();
+		copy.getFactory().setCharacterEscapes(new HtmlCharacterEscapes());
+		return new MappingJackson2HttpMessageConverter(copy);
 	}
 }
