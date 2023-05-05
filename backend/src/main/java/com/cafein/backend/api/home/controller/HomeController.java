5b953dd461273784ca85d643d3c3d787ca3f9442
euichanhwang <pine6971@naver.com>
2023-05-05 18:45:36 +0900

    refactor: Http 요청 본문의 내용을 읽을 수 없을 때 400 에러를 반환한다 #32

--- backend/src/main/java/com/cafein/backend/api/home/controller/HomeController.java
+++ backend/src/main/java/com/cafein/backend/api/home/controller/HomeController.java
@@ -3,6 +3,8 @@ package com.cafein.backend.api.home.controller;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.validation.Valid;
+
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -17,9 +19,13 @@ import com.cafein.backend.api.home.dto.HomeDTO;
 import com.cafein.backend.domain.cafe.service.CafeService;
 import com.cafein.backend.domain.member.entity.Member;
 import com.cafein.backend.domain.member.service.MemberService;
+import com.cafein.backend.global.resolver.MemberInfo;
+import com.cafein.backend.global.resolver.MemberInfoDTO;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
@@ -31,8 +37,9 @@ public class HomeController {
 	private final MemberService memberService;
 
 	@GetMapping("/home")
-	public ResponseEntity<HomeDTO.Response> home(@RequestBody HomeDTO.Request homeRequestDTO) {
-		Member findMember = memberService.getMemberByEmail(homeRequestDTO.getMemberEmail());
+	public ResponseEntity<HomeDTO.Response> home(final @Valid @RequestBody HomeDTO.Request homeRequestDTO,
+												 @MemberInfo MemberInfoDTO memberInfoDTO) {
+		Member findMember = memberService.findMemberByMemberId(memberInfoDTO.getMemberId());
 
 		Pageable pageable = PageRequest.of(
 			Integer.parseInt(homeRequestDTO.getPageNo()),
