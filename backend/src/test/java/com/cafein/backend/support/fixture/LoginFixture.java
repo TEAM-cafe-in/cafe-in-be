19e7884a9f5b71d8716cdb1c6c9e14b96e8b50ce
euichanhwang <pine6971@naver.com>
2023-05-21 14:29:14 +0900

    test: 테스트를 위한 픽스처를 생성한다 #8
    Co-authored-by: Seph-23 <seongmlyu95@gmail.com>

--- backend/src/test/java/com/cafein/backend/support/fixture/LoginFixture.java
+++ backend/src/test/java/com/cafein/backend/support/fixture/LoginFixture.java
@@ -3,16 +3,23 @@ package com.cafein.backend.support.fixture;
 import java.util.Date;
 
 import com.cafein.backend.api.login.dto.OAuthLoginDTO;
+import com.cafein.backend.api.token.dto.AccessTokenResponseDTO;
 
 public class LoginFixture {
 
 	public static final String ACCESS_TOKEN = "access_token";
-	public static final String AUTHORIZATION_HEADER = "Bearer access_token";
+	public static final String REFRESH_TOKEN = "refresh_token";
+	public static final String AUTHORIZATION_HEADER_ACCESS = "Bearer access_token";
+	public static final String AUTHORIZATION_HEADER_REFRESH = "Bearer refresh_token";
 	public static final String KAKAO_MEMBER_TYPE = "{\"memberType\":\"KAKAO\"}";
 	public static final String GOOGLE_MEMBER_TYPE = "{\"memberType\":\"GOOGLE\"}";
 
 	public static OAuthLoginDTO.Response kakaoLoginResponse() {
 		return createKakaoLoginResponse();
+	}
+
+	public static AccessTokenResponseDTO accessTokenResponse() {
+		return createAccessTokenResponse();
 	}
 
 	private static OAuthLoginDTO.Response createKakaoLoginResponse() {
@@ -22,6 +29,14 @@ public class LoginFixture {
 			.accessTokenExpireTime(new Date(System.currentTimeMillis()))
 			.refreshToken("refresh_token")
 			.refreshTokenExpireTime(new Date(System.currentTimeMillis()))
+			.build();
+	}
+
+	private static AccessTokenResponseDTO createAccessTokenResponse() {
+		return AccessTokenResponseDTO.builder()
+			.grantType("Bearer")
+			.accessToken("access_token")
+			.accessTokenExpireTime(new Date(System.currentTimeMillis()))
 			.build();
 	}
 }
