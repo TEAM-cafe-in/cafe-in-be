b71ee1d9689733f719f3f5af288b7be9d6e888da
euichanhwang <pine6971@naver.com>
2023-05-21 18:26:09 +0900

    test: 회원 정보 조회 컨트롤러 테스트를 작성한다 #41
    Co-authored-by: Seph-23 <seongmlyu95@gmail.com>

---
+++ backend/src/test/java/com/cafein/backend/api/MemberInfoControllerTest.java
@@ -0,0 +1,28 @@
+package com.cafein.backend.api;
+
+import static com.cafein.backend.support.fixture.LoginFixture.*;
+import static com.cafein.backend.support.fixture.MemberFixture.*;
+import static org.mockito.BDDMockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+
+class MemberInfoControllerTest extends ControllerTestSupporter {
+
+	@Test
+	void 회원_정보를_가져온다() throws Exception {
+		given(memberInfoService.getMemberInfo(any()))
+			.willReturn(memberInfoResponseDTO());
+
+		mockMvc.perform(get("/api/member/info")
+				.contentType(MediaType.APPLICATION_JSON_VALUE)
+				.header(HttpHeaders.AUTHORIZATION, AUTHORIZATION_HEADER_ACCESS)
+			)
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.memberId").value(1))
+			.andExpect(jsonPath("$.email").value("test@test.com"));
+	}
+}
