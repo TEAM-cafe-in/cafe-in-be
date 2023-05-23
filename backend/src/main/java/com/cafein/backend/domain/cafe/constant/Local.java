5fa9d7fb636cd9a1ced0f75406e3dd3eca7dcf9c
euichanhwang <pine6971@naver.com>
2023-05-23 22:56:21 +0900

    feat: 지역을 잘못 입력 시 BusinessException을 반환한다  #28
    Co-authored-by: Seph-23 <seongmlyu95@gmail.com>

--- backend/src/main/java/com/cafein/backend/domain/cafe/constant/Local.java
+++ backend/src/main/java/com/cafein/backend/domain/cafe/constant/Local.java
@@ -1,11 +1,17 @@
 package com.cafein.backend.domain.cafe.constant;
 
+import com.cafein.backend.global.error.ErrorCode;
+import com.cafein.backend.global.error.exception.BusinessException;
 
 public enum Local {
 
 	HONGDAE, YEONNAM, SEONGSU;
 
 	public static Local from(String local) {
-		return Local.valueOf(local.toUpperCase());
+		try {
+			return Local.valueOf(local.toUpperCase());
+		} catch (IllegalArgumentException e) {
+			throw new BusinessException(ErrorCode.LOCAL_NOT_EXIST);
+		}
 	}
 }
