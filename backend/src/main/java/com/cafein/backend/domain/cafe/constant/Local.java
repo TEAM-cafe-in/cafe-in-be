a5b2d5cf7b24f9fa07589151cca7fa9574592213
euichanhwang <pine6971@naver.com>
2023-05-23 19:05:41 +0900

    refactor: 입력한 지역 정보로 지역 엔티티를 반환하는 메서드를 추가한다 #28
    Co-authored-by: Seph-23 <seongmlyu95@gmail.com>

--- backend/src/main/java/com/cafein/backend/domain/cafe/constant/Local.java
+++ backend/src/main/java/com/cafein/backend/domain/cafe/constant/Local.java
@@ -4,4 +4,8 @@ package com.cafein.backend.domain.cafe.constant;
 public enum Local {
 
 	HONGDAE, YEONNAM, SEONGSU;
+
+	public static Local from(String local) {
+		return Local.valueOf(local.toUpperCase());
+	}
 }
