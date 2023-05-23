9132c4ed7c015a9b350a606f057769c3959bbea7
euichanhwang <pine6971@naver.com>
2023-05-23 19:06:08 +0900

    refactor: 사용하지 않는 주석을 제거한다 #28
    Co-authored-by: Seph-23 <seongmlyu95@gmail.com>

--- backend/src/main/java/com/cafein/backend/domain/cafe/service/CafeService.java
+++ backend/src/main/java/com/cafein/backend/domain/cafe/service/CafeService.java
@@ -2,15 +2,12 @@ package com.cafein.backend.domain.cafe.service;
 
 import java.util.List;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.cafein.backend.domain.cafe.constant.Local;
 import com.cafein.backend.domain.cafe.entity.Cafe;
 import com.cafein.backend.domain.cafe.repository.CafeRepository;
-import com.cafein.backend.global.error.ErrorCode;
-import com.cafein.backend.global.error.exception.EntityNotFoundException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -18,31 +15,11 @@ import lombok.RequiredArgsConstructor;
 @Transactional
 @RequiredArgsConstructor
 public class CafeService {
-	//TODO CafeServiceTest 작성
+
 	private final CafeRepository cafeRepository;
 
-	// public Cafe registerCafe(Cafe cafe) {
-	// 	return cafeRepository.save(cafe);
-	// }
-	//
-	// @Transactional(readOnly = true)
-	// public Cafe findOne(Long cafeId) {
-	// 	return cafeRepository.findById(cafeId)
-	// 		.orElseThrow(() -> new EntityNotFoundException(ErrorCode.CAFE_NOT_EXIST));
-	// }
-	//
-	// @Transactional(readOnly = true)
-	// public Page<Cafe> findCafeByLocal(Pageable pageable) {
-	// 	return cafeRepository.findCafeByLocal(pageable);
-	// }
-
-	// @Transactional(readOnly = true)
-	// public List<Cafe> findAll() {
-	// 	return cafeRepository.findAll();
-	// }
-
 	@Transactional(readOnly = true)
-	public Page<Cafe> findAll(Pageable pageable) {
-		return cafeRepository.findAll(pageable);
+	public List<Cafe> findAllByLocal(Local local) {
+		return cafeRepository.findAllByLocal(local);
 	}
 }
