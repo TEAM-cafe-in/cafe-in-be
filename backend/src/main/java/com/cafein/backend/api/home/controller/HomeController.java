945ca0a4ad5e5b974ab9abf13b01ac912a0a281a
euichanhwang <pine6971@naver.com>
2023-05-23 19:07:29 +0900

    feat: 지역을 기반으로 카페를 조회하는 메서드를 구현한다 #28
    Co-authored-by: Seph-23 <seongmlyu95@gmail.com>

--- backend/src/main/java/com/cafein/backend/api/home/controller/HomeController.java
+++ backend/src/main/java/com/cafein/backend/api/home/controller/HomeController.java
@@ -5,9 +5,6 @@ import java.util.stream.Collectors;
 
 import javax.validation.Valid;
 
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.cafein.backend.api.home.dto.CafeDTO;
 import com.cafein.backend.api.home.dto.HomeDTO;
+import com.cafein.backend.domain.cafe.constant.Local;
 import com.cafein.backend.domain.cafe.service.CafeService;
 import com.cafein.backend.domain.member.entity.Member;
 import com.cafein.backend.domain.member.service.MemberService;
@@ -31,23 +29,15 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/api")
 public class HomeController {
 
-	private static final int PAGE_SIZE = 5;
-
 	private final CafeService cafeService;
 	private final MemberService memberService;
 
 	@GetMapping("/home")
 	public ResponseEntity<HomeDTO.Response> home(final @Valid @RequestBody HomeDTO.Request homeRequestDTO,
 												 @MemberInfo MemberInfoDTO memberInfoDTO) {
-		Member findMember = memberService.findMemberByMemberId(memberInfoDTO.getMemberId());
+		final Member findMember = memberService.findMemberByMemberId(memberInfoDTO.getMemberId());
 
-		Pageable pageable = PageRequest.of(
-			Integer.parseInt(homeRequestDTO.getPageNo()),
-			PAGE_SIZE,
-			Sort.by(homeRequestDTO.getSortBy()).descending()
-		);
-
-		List<CafeDTO> cafes = cafeService.findAll(pageable)
+		final List<CafeDTO> cafes = cafeService.findAllByLocal(Local.from(homeRequestDTO.getLocal()))
 			.stream()
 			.map(CafeDTO::of)
 			.collect(Collectors.toList());
--- backend/src/main/java/com/cafein/backend/domain/cafe/repository/CafeRepository.java
+++ backend/src/main/java/com/cafein/backend/domain/cafe/repository/CafeRepository.java
@@ -1,14 +1,15 @@
 package com.cafein.backend.domain.cafe.repository;
 
+import java.util.List;
 import java.util.Optional;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.cafein.backend.domain.cafe.constant.Local;
 import com.cafein.backend.domain.cafe.entity.Cafe;
 
 public interface CafeRepository extends JpaRepository<Cafe, Long> {
 
 	Optional<Cafe> findByName(String name);
+	List<Cafe> findAllByLocal(Local local);
 }
