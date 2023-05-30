84aa3ccf0dbef3988c292886fbb3dcb4de6bd024
euichanhwang <pine6971@naver.com>
2023-05-30 19:55:18 +0900

    refactor: 로그인 픽스처를 수정한다.
    클린 코드 책에 따라 public static 상수 이후에 공백으로 인스턴스와 구분한다

--- backend/src/test/java/com/cafein/backend/support/fixture/LoginFixture.java
+++ backend/src/test/java/com/cafein/backend/support/fixture/LoginFixture.java
@@ -4,6 +4,8 @@ import java.util.Date;
 
 import com.cafein.backend.api.login.dto.OAuthLoginDTO;
 import com.cafein.backend.api.token.dto.AccessTokenResponseDTO;
+import com.cafein.backend.domain.member.constant.MemberType;
+import com.cafein.backend.external.oauth.model.OAuthAttributes;
 
 public class LoginFixture {
 
@@ -14,15 +16,30 @@ public class LoginFixture {
 	public static final String KAKAO_MEMBER_TYPE = "{\"memberType\":\"KAKAO\"}";
 	public static final String GOOGLE_MEMBER_TYPE = "{\"memberType\":\"GOOGLE\"}";
 
-	public static OAuthLoginDTO.Response kakaoLoginResponse() {
-		return createKakaoLoginResponse();
+	public static final OAuthLoginDTO.Response KAKAO_LOGIN_RESPONSE = kakaoLoginResponse();
+	public static final AccessTokenResponseDTO ACCESS_TOKEN_RESPONSE = accessTokenResponse();
+	public static final OAuthAttributes KAKAO_OAUTH_ATTRIBUTES = createKakaoOauthAttributes();
+	public static final OAuthAttributes GOOGLE_OAUTH_ATTRIBUTES = createGoogleOauthAttributes();
+
+	private static OAuthAttributes createKakaoOauthAttributes() {
+		return OAuthAttributes.builder()
+			.name("chan")
+			.email("test@cafein.com")
+			.profile("profile")
+			.memberType(MemberType.KAKAO)
+			.build();
 	}
 
-	public static AccessTokenResponseDTO accessTokenResponse() {
-		return createAccessTokenResponse();
+	private static OAuthAttributes createGoogleOauthAttributes() {
+		return OAuthAttributes.builder()
+			.name("chan")
+			.email("test@cafein.com")
+			.profile("profile")
+			.memberType(MemberType.GOOGLE)
+			.build();
 	}
 
-	private static OAuthLoginDTO.Response createKakaoLoginResponse() {
+	private static OAuthLoginDTO.Response kakaoLoginResponse() {
 		return OAuthLoginDTO.Response.builder()
 			.grantType("Bearer")
 			.accessToken("access_token")
@@ -32,7 +49,7 @@ public class LoginFixture {
 			.build();
 	}
 
-	private static AccessTokenResponseDTO createAccessTokenResponse() {
+	private static AccessTokenResponseDTO accessTokenResponse() {
 		return AccessTokenResponseDTO.builder()
 			.grantType("Bearer")
 			.accessToken("access_token")
