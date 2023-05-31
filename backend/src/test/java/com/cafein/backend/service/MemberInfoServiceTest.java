f56a48b77bbb451f636811ce6c461bcd5a8b90a1
euichanhwang <pine6971@naver.com>
2023-05-31 12:30:44 +0900

    refactor: 픽스처로 생성한 회원을 사용하도록 변경한다 #50
    Co-authored-by: Seph-23 <seongmlyu95@gmail.com>

--- backend/src/test/java/com/cafein/backend/service/MemberInfoServiceTest.java
+++ backend/src/test/java/com/cafein/backend/service/MemberInfoServiceTest.java
@@ -1,8 +1,8 @@
 package com.cafein.backend.service;
 
+import static com.cafein.backend.support.fixture.MemberFixture.*;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
-import static org.springframework.test.util.ReflectionTestUtils.*;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,31 +10,26 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 
 import com.cafein.backend.api.member.dto.MemberInfoResponseDTO;
 import com.cafein.backend.api.member.service.MemberInfoService;
-import com.cafein.backend.domain.member.entity.Member;
 import com.cafein.backend.domain.member.service.MemberService;
-import com.cafein.backend.support.fixture.MemberFixture;
 import com.cafein.backend.support.utils.DataBaseSupporter;
 import com.cafein.backend.support.utils.ServiceTest;
 
 @ServiceTest
 class MemberInfoServiceTest extends DataBaseSupporter {
 
-	@MockBean
-	private MemberService memberService;
-
 	@Autowired
 	private MemberInfoService memberInfoService;
 
+	@MockBean
+	private MemberService memberService;
+
 	@Test
 	void 회원_정보를_가져온다() {
-		Member createMember = MemberFixture.createMember();
-		setField(createMember, "memberId", 1L);
-		given(memberService.findMemberByMemberId(anyLong()))
-			.willReturn(createMember);
+		given(memberService.findMemberByMemberId(anyLong())).willReturn(MEMBER);
 
-		MemberInfoResponseDTO findMember = memberInfoService.getMemberInfo(createMember.getMemberId());
+		MemberInfoResponseDTO memberInfoResponseDTO = memberInfoService.getMemberInfo(MEMBER.getMemberId());
 
 		then(memberService).should(times(1)).findMemberByMemberId(anyLong());
-		assertThat(findMember.getMemberId()).isNotNull();
+		assertThat(memberInfoResponseDTO.getMemberId()).isNotNull();
 	}
 }
--- backend/src/test/java/com/cafein/backend/service/TokenServiceTest.java
+++ backend/src/test/java/com/cafein/backend/service/TokenServiceTest.java
@@ -1,13 +1,10 @@
 package com.cafein.backend.service;
 
+import static com.cafein.backend.support.fixture.MemberFixture.*;
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
-import static org.springframework.test.util.ReflectionTestUtils.*;
 
-import java.time.LocalDateTime;
 import java.util.Date;
-import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,16 +13,15 @@ import org.springframework.boot.test.mock.mockito.SpyBean;
 
 import com.cafein.backend.api.token.dto.AccessTokenResponseDTO;
 import com.cafein.backend.api.token.service.TokenService;
-import com.cafein.backend.domain.member.entity.Member;
-import com.cafein.backend.domain.member.repository.MemberRepository;
 import com.cafein.backend.domain.member.service.MemberService;
-import com.cafein.backend.global.jwt.constant.GrantType;
 import com.cafein.backend.global.jwt.service.TokenManager;
-import com.cafein.backend.support.fixture.MemberFixture;
 import com.cafein.backend.support.utils.ServiceTest;
 
 @ServiceTest
 class TokenServiceTest {
+
+	@Autowired
+	private TokenService tokenService;
 
 	@MockBean
 	private MemberService memberService;
@@ -33,30 +29,18 @@ class TokenServiceTest {
 	@SpyBean
 	private TokenManager tokenManager;
 
-	@Autowired
-	private TokenService tokenService;
-
 	@Test
-	void 액세스_토큰_재발급_테스트() {
-		Member createMember = MemberFixture.createMember();
-		setField(createMember, "memberId", 1L);
-		setField(createMember, "refreshToken", "refresh_token");
-		setField(createMember, "tokenExpirationTime", LocalDateTime.now().plusDays(15));
-		given(memberService.findMemberByRefreshToken(anyString()))
-			.willReturn(createMember);
-		given(tokenManager.createAccessTokenExpireTime())
-			.willReturn(new Date(System.currentTimeMillis() + Long.parseLong("900000")));
+	void access_Token_재발급을_테스트한다() {
+		given(memberService.findMemberByRefreshToken(anyString())).willReturn(MEMBER);
+		given(tokenManager.createAccessTokenExpireTime()).willReturn(
+			new Date(System.currentTimeMillis() + Long.parseLong("900000")));
 
-		AccessTokenResponseDTO accessTokenByRefreshToken = tokenService.
-			createAccessTokenByRefreshToken(createMember.getRefreshToken());
+		AccessTokenResponseDTO accessTokenResponseDTO = tokenService.
+			createAccessTokenByRefreshToken(MEMBER.getRefreshToken());
 
-		then(memberService).should(times(1)).findMemberByRefreshToken(anyString());
-		then(tokenManager).should(times(1)).createAccessTokenExpireTime();
 		then(tokenManager).should(times(1)).createAccessToken(anyLong(), any(), any(Date.class));
-		assertAll(
-			() -> assertThat(accessTokenByRefreshToken.getGrantType().toUpperCase()).isEqualTo(GrantType.BEARER.toString()),
-			() -> assertThat(accessTokenByRefreshToken.getAccessToken()).isNotNull(),
-			() -> assertThat(accessTokenByRefreshToken.getAccessTokenExpireTime()).isAfter(new Date(System.currentTimeMillis()))
-		);
+
+		assertThat(accessTokenResponseDTO.getAccessToken()).isNotNull();
+		assertThat(accessTokenResponseDTO.getAccessTokenExpireTime()).isAfter(new Date(System.currentTimeMillis()));
 	}
 }
