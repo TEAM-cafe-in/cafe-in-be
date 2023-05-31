7fcf46eb45d4c73e6cbca134d868b2cdc3453d9a
euichanhwang <pine6971@naver.com>
2023-05-31 14:35:15 +0900

    test: 회원 refresh token 관련 로직을 테스트한다 #52

--- backend/src/test/java/com/cafein/backend/support/fixture/LoginFixture.java
+++ backend/src/test/java/com/cafein/backend/support/fixture/LoginFixture.java
@@ -6,6 +6,7 @@ import com.cafein.backend.api.login.dto.OAuthLoginDTO;
 import com.cafein.backend.api.token.dto.AccessTokenResponseDTO;
 import com.cafein.backend.domain.member.constant.MemberType;
 import com.cafein.backend.external.oauth.model.OAuthAttributes;
+import com.cafein.backend.global.jwt.dto.JwtTokenDTO;
 
 public class LoginFixture {
 
@@ -21,6 +22,7 @@ public class LoginFixture {
 	public static final AccessTokenResponseDTO ACCESS_TOKEN_RESPONSE = accessTokenResponse();
 	public static final OAuthAttributes KAKAO_OAUTH_ATTRIBUTES = createKakaoOauthAttributes();
 	public static final OAuthAttributes GOOGLE_OAUTH_ATTRIBUTES = createGoogleOauthAttributes();
+	public static final JwtTokenDTO JWT_TOKEN_DTO = createJwtTokenDTO();
 
 	private static OAuthAttributes createKakaoOauthAttributes() {
 		return OAuthAttributes.builder()
@@ -55,6 +57,16 @@ public class LoginFixture {
 			.grantType("Bearer")
 			.accessToken("access_token")
 			.accessTokenExpireTime(new Date(System.currentTimeMillis()))
+			.build();
+	}
+
+	private static JwtTokenDTO createJwtTokenDTO() {
+		return JwtTokenDTO.builder()
+			.grantType("Bearer")
+			.accessToken(ACCESS_TOKEN)
+			.accessTokenExpireTime(new Date(System.currentTimeMillis() + Long.parseLong("900000")))
+			.refreshToken(REFRESH_TOKEN)
+			.refreshTokenExpireTime(new Date(System.currentTimeMillis() + Long.parseLong("1209600000")))
 			.build();
 	}
 }
