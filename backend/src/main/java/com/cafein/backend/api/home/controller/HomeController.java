4ee590bc40d0d6a75000305a0804f7fb4ed216a9
Seph-23 <seongmlyu95@gmail.com>
2023-06-04 17:26:07 +0900

    feat: 홈 화면 데이터를 가져오는 Controller를 구현한다
    Co-authored-by: euichanhwang <pine6971@naver.com>

--- backend/src/main/java/com/cafein/backend/api/home/controller/HomeController.java
+++ backend/src/main/java/com/cafein/backend/api/home/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.cafein.backend.api.home.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.http.ResponseEntity;
@@ -11,10 +12,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cafein.backend.api.home.dto.HomeDTO;
-import com.cafein.backend.domain.cafe.repository.CafeRepositoryImpl;
+import com.cafein.backend.api.home.dto.HomeResponseDTO;
 import com.cafein.backend.domain.cafe.service.CafeService;
-import com.cafein.backend.global.resolver.MemberInfo;
-import com.cafein.backend.global.resolver.MemberInfoDTO;
+import com.cafein.backend.global.util.AuthorizationHeaderUtils;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -26,13 +26,12 @@ import lombok.extern.slf4j.Slf4j;
 public class HomeController {
 
 	private final CafeService cafeService;
-	private final CafeRepositoryImpl cafeRepository;
 
 	@GetMapping("/home")
-	public ResponseEntity<HomeDTO.Response> home(final @Valid @RequestBody HomeDTO.Request homeRequestDTO,
-												 final @MemberInfo MemberInfoDTO memberInfoDTO) {
-		final List<HomeDTO.Response> homeDTO = cafeRepository.findHomeDTO();
-		System.out.println(homeDTO);
-		return ResponseEntity.ok().build();
+	public ResponseEntity<List<HomeResponseDTO>> home(final @Valid @RequestBody HomeDTO.Request homeRequestDTO,
+												 HttpServletRequest httpServletRequest) {
+		String authorizationHeader = httpServletRequest.getHeader("Authorization");
+		AuthorizationHeaderUtils.validateAuthorization(authorizationHeader);
+		return ResponseEntity.ok(List.copyOf(cafeService.getHomeDTO(homeRequestDTO.getLocal())));
 	}
 }
