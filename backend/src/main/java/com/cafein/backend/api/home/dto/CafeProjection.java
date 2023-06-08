cc980b597c51aa4d82fdf176a039923a85929379
euichanhwang <pine6971@naver.com>
2023-06-08 14:12:54 +0900

    feat: 지역 별 카페의 총 수를 반환하는 필드를 추가한다 #28
    Co-authored-by: Seph-23 <seongmlyu95@gmail.com>

---
+++ backend/src/main/java/com/cafein/backend/api/home/dto/CafeProjection.java
@@ -0,0 +1,12 @@
+
+package com.cafein.backend.api.home.dto;
+
+public interface CafeProjection {
+
+	String getName();
+	String getPhoneNumber();
+	String getAddress();
+	String getCommentReviewCount();
+	String getStatus();
+	String getAverageCongestion();
+}
