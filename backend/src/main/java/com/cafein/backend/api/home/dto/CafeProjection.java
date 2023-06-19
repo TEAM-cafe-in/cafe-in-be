d1085ab042aadd546283c85e52c4764f60af4b83
Seph-23 <seongmlyu95@gmail.com>
2023-06-19 11:30:42 +0900

    refactor: Swagger Documentation을 추가한다.
    Co-authored-by: euichanhwang <pine6971@naver.com>

--- backend/src/main/java/com/cafein/backend/api/home/dto/CafeProjection.java
+++ backend/src/main/java/com/cafein/backend/api/home/dto/CafeProjection.java
@@ -1,8 +1,11 @@
 
 package com.cafein.backend.api.home.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public interface CafeProjection {
 
+	@Schema(description = "카페 이름", example = "5to7", required = true)
 	String getName();
 	String getPhoneNumber();
 	String getAddress();
--- backend/src/main/java/com/cafein/backend/api/home/dto/HomeResponseDTO.java
+++ backend/src/main/java/com/cafein/backend/api/home/dto/HomeResponseDTO.java
@@ -2,12 +2,14 @@ package com.cafein.backend.api.home.dto;
 
 import java.util.List;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter @Builder
 public class HomeResponseDTO {
 
+	@Schema(description = "총 카페 수", example = "60", required = true)
 	private long cafeCount;
 
 	private List<CafeProjection> cafes;
