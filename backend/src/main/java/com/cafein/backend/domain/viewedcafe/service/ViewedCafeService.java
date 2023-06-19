a7c5d5f78a8a4d8e4aeb6aba83f9d5dea287f5e2
Seph-23 <seongmlyu95@gmail.com>
2023-06-19 19:29:12 +0900

    refactor: ViewedCafe를 추가하는 메서드를 구현한다
    Co-authored-by: euichanhwang <pine6971@naver.com>

--- backend/src/main/java/com/cafein/backend/domain/viewedcafe/service/ViewedCafeService.java
+++ backend/src/main/java/com/cafein/backend/domain/viewedcafe/service/ViewedCafeService.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.cafein.backend.domain.member.entity.Member;
+import com.cafein.backend.domain.viewedcafe.entity.ViewedCafe;
 import com.cafein.backend.domain.viewedcafe.repository.ViewedCafeRepository;
 
 import lombok.RequiredArgsConstructor;
