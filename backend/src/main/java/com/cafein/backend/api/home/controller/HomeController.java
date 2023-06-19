aeb7cca3e54539a10e8cdc8cc1276da965245348
Seph-23 <seongmlyu95@gmail.com>
2023-06-19 11:31:05 +0900

    refactor: Swagger Documentation을 추가한다.
    Co-authored-by: euichanhwang <pine6971@naver.com>

--- backend/src/main/java/com/cafein/backend/api/home/controller/HomeController.java
+++ backend/src/main/java/com/cafein/backend/api/home/controller/HomeController.java
@@ -11,9 +11,14 @@ import com.cafein.backend.api.home.dto.HomeResponseDTO;
 import com.cafein.backend.domain.cafe.service.CafeService;
 import com.cafein.backend.global.util.AuthorizationHeaderUtils;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+@Tag(name = "home", description = "홈 화면 API")
 @Slf4j
 @RestController
 @RequiredArgsConstructor
@@ -22,6 +27,9 @@ public class HomeController {
 
 	private final CafeService cafeService;
 
+	@Tag(name = "home")
+	@Operation(summary = "홈 화면 API",
+		description = "홈 화면에 필요한 모든 카페 정보를 반환하는 API 입니다.")
 	@GetMapping("/home")
 	public ResponseEntity<HomeResponseDTO> home(HttpServletRequest httpServletRequest) {
 		String authorizationHeader = httpServletRequest.getHeader("Authorization");
