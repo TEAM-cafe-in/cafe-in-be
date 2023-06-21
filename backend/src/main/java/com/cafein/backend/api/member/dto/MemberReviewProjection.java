e29775185f873a193ffbd98a975fa3ddd23b95e0
Seph-23 <seongmlyu95@gmail.com>
2023-06-21 22:08:08 +0900

    feat: MemberProfile API를 구현한다.
    Co-authored-by: euichanhwang <pine6971@naver.com>

---
+++ backend/src/main/java/com/cafein/backend/api/member/dto/MemberReviewProjection.java
@@ -0,0 +1,10 @@
+package com.cafein.backend.api.member.dto;
+
+public interface MemberReviewProjection {
+
+	String getCafeName();
+	String getAddress();
+	String getCafeCongestion();
+	String getIsClean();
+	String getHasPlug();
+}
---
+++ backend/src/main/java/com/cafein/backend/api/member/service/MyPageService.java
@@ -0,0 +1,36 @@
+package com.cafein.backend.api.member.service;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.cafein.backend.api.member.dto.MyPageDTO;
+import com.cafein.backend.domain.Review.respository.ReviewRepository;
+import com.cafein.backend.domain.cafe.service.CafeService;
+import com.cafein.backend.domain.viewedcafe.service.ViewedCafeService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class MyPageService {
+
+	private final CafeService cafeService;
+	private final ViewedCafeService viewedCafeService;
+	private final ReviewRepository reviewRepository;
+
+	@Transactional(readOnly = true)
+	public MyPageDTO getMyPageDTO(Long memberId) {
+		final List<Long> viewedCafeIds = viewedCafeService.findViewedCafes(memberId);
+		log.debug("memberId = {}" , memberId);
+		log.debug("viewedCafeIds = {}" , viewedCafeIds);
+		return MyPageDTO.of(cafeService.findCafeInfoViewedByMember(viewedCafeIds),
+			reviewRepository.findReviewsByMemberId(memberId),
+			reviewRepository.countReviewByMemberId(memberId));
+	}
+}
