63d9b206ace53fe1a6eb1d5ba1e7b5e3fefc935a
euichanhwang <pine6971@naver.com>
2023-06-25 16:38:49 +0900

    test: 회원 조회 및 마이페이지 API를 테스트한다 #66

--- backend/src/test/java/com/cafein/backend/api/MemberControllerTest.java
+++ backend/src/test/java/com/cafein/backend/api/MemberControllerTest.java
@@ -1,28 +1,56 @@
 package com.cafein.backend.api;
 
-import static com.cafein.backend.support.fixture.LoginFixture.*;
 import static com.cafein.backend.support.fixture.MemberFixture.*;
+import static org.assertj.core.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-class MemberControllerTest extends ControllerTestSupporter {
+import com.cafein.backend.api.member.controller.MemberController;
+import com.cafein.backend.api.member.dto.MemberInfoResponseDTO;
+import com.cafein.backend.api.member.dto.MyPageDTO;
+import com.cafein.backend.api.member.service.MemberInfoService;
+import com.cafein.backend.api.member.service.MyPageService;
+
+@ExtendWith(MockitoExtension.class)
+class MemberControllerTest {
+
+	@Mock
+	private MemberInfoService memberInfoService;
+
+	@Mock
+	private MyPageService myPageService;
+
+	@InjectMocks
+	private MemberController memberController;
 
 	@Test
-	void 회원_정보를_가져온다() throws Exception {
+	void 회원_정보를_가져온다() {
 		given(memberInfoService.getMemberInfo(any()))
 			.willReturn(MEMBER_INFO_RESPONSE_DTO);
 
-		mockMvc.perform(get("/api/member/info")
-				.contentType(MediaType.APPLICATION_JSON_VALUE)
-				.header(HttpHeaders.AUTHORIZATION, AUTHORIZATION_HEADER_ACCESS)
-			)
-			.andExpect(status().isOk())
-			.andExpect(jsonPath("$.memberId").value(1))
-			.andExpect(jsonPath("$.email").value("test@test.com"));
+		final ResponseEntity<MemberInfoResponseDTO> result = memberController.getMemberInfo(MEMBER_INFO_DTO);
+
+		assertThat(result.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(result.getBody()).isEqualTo(MEMBER_INFO_RESPONSE_DTO);
+	}
+
+	@Test
+	void 마이페이지에_사용하는_회원_정보를_반환한다() {
+		MyPageDTO response = MyPageDTO.builder()
+			.reviewCount(5L)
+			.build();
+		given(myPageService.getMyPageDTO(1L)).willReturn(response);
+
+		final ResponseEntity<MyPageDTO> result = memberController.getMyPage(MEMBER_INFO_DTO);
+
+		assertThat(result.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(result.getBody()).isEqualTo(response);
 	}
 }
