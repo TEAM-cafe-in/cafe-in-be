ebcbde7f60f3b5baac3ef8b12a7c8beca7f61122
euichanhwang <pine6971@naver.com>
2023-06-28 10:11:51 +0900

    feat: 카페에 댓글을 추가하는 API를 구현한다  #67
    `키워드`는 중복 선택이 가능하도록 한다
    
    Co-authored-by: Seph-23 <seongmlyu95@gmail.com>

---
+++ backend/src/main/java/com/cafein/backend/api/comment/controller/CommentController.java
@@ -0,0 +1,46 @@
+package com.cafein.backend.api.comment.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cafein.backend.api.comment.dto.CommentDTO;
+import com.cafein.backend.domain.comment.service.CommentService;
+import com.cafein.backend.global.resolver.MemberInfo;
+import com.cafein.backend.global.resolver.MemberInfoDTO;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import springfox.documentation.annotations.ApiIgnore;
+
+@Tag(name = "cafe", description = "카페 관련 API")
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class CommentController {
+
+	private final CommentService commentService;
+
+	@Tag(name = "cafe")
+	@Operation(summary = "카페 댓글 등록 API", description = "카페에 댓글을 추가하는 API 입니다. 키워드는 중복 선택이 가능합니다.")
+	@ApiResponses({
+		@ApiResponse(responseCode = "CO-001", description = "해당 키워드는 존재하지 않습니다.")
+	})
+	@PostMapping("/cafe/{cafeId}/comment")
+	public ResponseEntity<String> addComment(@Valid @RequestBody CommentDTO.Request requestDTO,
+										     @ApiIgnore @MemberInfo MemberInfoDTO memberInfoDTO,
+										     @PathVariable Long cafeId) {
+		commentService.addComment(requestDTO, cafeId, memberInfoDTO.getMemberId());
+		return ResponseEntity.ok("comment added");
+	}
+}
--- backend/src/main/java/com/cafein/backend/domain/comment/service/CommentService.java
+++ backend/src/main/java/com/cafein/backend/domain/comment/service/CommentService.java
@@ -1,24 +1,56 @@
 package com.cafein.backend.domain.comment.service;
 
-import java.util.List;
-
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.cafein.backend.api.comment.dto.CommentDTO;
+import com.cafein.backend.domain.cafe.entity.Cafe;
+import com.cafein.backend.domain.cafe.service.CafeService;
+import com.cafein.backend.domain.comment.constant.Keyword;
 import com.cafein.backend.domain.comment.entity.Comment;
 import com.cafein.backend.domain.comment.repository.CommentRepository;
+import com.cafein.backend.domain.commentkeyword.entity.CommentKeyword;
+import com.cafein.backend.domain.commentkeyword.repository.CommentKeywordRepository;
+import com.cafein.backend.domain.member.entity.Member;
+import com.cafein.backend.domain.member.service.MemberService;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class CommentService {
 
 	private final CommentRepository commentRepository;
+	private final CommentKeywordRepository commentKeywordRepository;
+	private final MemberService memberService;
+	private final CafeService cafeService;
 
-	@Transactional(readOnly = true)
-	public Integer countByCafeId(Long cafeId) {
-		return commentRepository.countCommentByCafeId(cafeId);
+	public void addComment(final CommentDTO.Request requestDTO, final Long cafeId, final Long memberId) {
+		Comment comment = createCafeComment(requestDTO, cafeId, memberId);
+		commentRepository.save(comment);
+	}
+
+	private Comment createCafeComment(final CommentDTO.Request requestDTO, final Long cafeId, final Long memberId) {
+		final Member member = memberService.findMemberByMemberId(memberId);
+		final Cafe cafe = cafeService.findCafeByCafeId(cafeId);
+		Comment comment = Comment.builder()
+			.content(requestDTO.getContent())
+			.cafe(cafe)
+			.member(member)
+			.build();
+		for (String key : requestDTO.getKeywords()) {
+			commentKeywordRepository.save(createCommentKeyword(comment, key));
+		}
+		return comment;
+	}
+
+	private CommentKeyword createCommentKeyword(final Comment comment, final String key) {
+		return CommentKeyword.builder()
+			.keyword(Keyword.from(key))
+			.comment(comment)
+			.build();
 	}
 }
