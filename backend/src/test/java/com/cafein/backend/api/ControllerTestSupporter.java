e292a249716a4665d2ceac29083554060953b874
euichanhwang <pine6971@naver.com>
2023-07-10 23:15:27 +0900

    feat: `@WebMvcTest`를 사용하지 않는 컨트롤러 테스트 설정 파일을 생성한다 #82
    MemberInfoArgumentResolver를 Mocking한다.
    MemberInfoArgumentResolver를 사용하지 않는 클래스가 존재하기 때문에 lenient()를 사용해 loose stubbing을 처리한다.

--- backend/src/test/java/com/cafein/backend/api/ControllerTestSupporter.java
+++ backend/src/test/java/com/cafein/backend/api/ControllerTestSupporter.java
@@ -1,30 +1,39 @@
 package com.cafein.backend.api;
 
 import static com.cafein.backend.support.fixture.MemberFixture.*;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.BDDMockito;
 import org.mockito.Mock;
-import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import com.cafein.backend.global.error.GlobalExceptionHandler;
-import com.cafein.backend.global.interceptor.AuthenticationInterceptor;
 import com.cafein.backend.global.resolver.MemberInfoArgumentResolver;
 
 @ExtendWith(MockitoExtension.class)
 public abstract class ControllerTestSupporter {
 
+	@Mock
+	private MemberInfoArgumentResolver memberInfoArgumentResolver;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		lenient().when(memberInfoArgumentResolver.supportsParameter(any()))
+			.thenReturn(true);
+
+		lenient().when(memberInfoArgumentResolver.resolveArgument(any(), any(), any(), any()))
+			.thenReturn(MEMBER_INFO_DTO);
+	}
+
 	protected MockMvc mockMvc(Object controller) {
 		return MockMvcBuilders.standaloneSetup(controller)
 			.setControllerAdvice(new GlobalExceptionHandler())
+			.setCustomArgumentResolvers(memberInfoArgumentResolver)
 			.alwaysDo(MockMvcResultHandlers.print())
 			.build();
 	}
