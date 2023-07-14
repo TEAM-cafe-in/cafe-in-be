f36f7f9d4f9200f8be65cdb3738beabeec482b15
euichanhwang <pine6971@naver.com>
2023-07-14 23:41:27 +0900

    feat: 댓글을 수정 테스트를 작성한다 #86

--- backend/src/test/java/com/cafein/backend/service/CommentServiceTest.java
+++ backend/src/test/java/com/cafein/backend/service/CommentServiceTest.java
@@ -8,14 +8,12 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.jdbc.Sql;
 
-import com.cafein.backend.domain.cafe.service.CafeService;
+import com.cafein.backend.domain.comment.entity.Comment;
 import com.cafein.backend.domain.comment.repository.CommentRepository;
 import com.cafein.backend.domain.comment.service.CommentService;
-import com.cafein.backend.domain.member.service.MemberService;
 import com.cafein.backend.global.error.exception.EntityNotFoundException;
 import com.cafein.backend.support.utils.DataBaseSupporter;
 import com.cafein.backend.support.utils.ServiceTest;
@@ -30,12 +28,6 @@ class CommentServiceTest extends DataBaseSupporter {
 	@SpyBean
 	private CommentRepository commentRepository;
 
-	@MockBean
-	private MemberService memberService;
-
-	@MockBean
-	private CafeService cafeService;
-
 	@Test
 	void 댓글을_등록한다() {
 		final CommentRequest commentRequest = CommentRequest.builder()
@@ -44,9 +36,24 @@ class CommentServiceTest extends DataBaseSupporter {
 			.build();
 
 		final Long commentId = commentService.addComment(commentRequest, 1L, 1L);
+		final Comment comment = commentRepository.findById(2L).get();
 
 		assertThat(commentId).isEqualTo(2L);
+		assertThat(comment.getContent()).isEqualTo("테스트 댓글");
 		then(commentRepository).should(times(1)).save(any());
+	}
+
+	@Test
+	void 댓글을_수정한다() {
+		final CommentRequest commentRequest = CommentRequest.builder()
+			.content("테스트 댓글 수정")
+			.keywords(List.of("좌석", "분위기"))
+			.build();
+
+		commentService.updateComment(commentRequest, 1L, 1L);
+
+		final Comment comment = commentRepository.findById(1L).get();
+		assertThat(comment.getContent()).isEqualTo("테스트 댓글 수정");
 	}
 
 	@Test
