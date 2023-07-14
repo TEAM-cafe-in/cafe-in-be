f59fa59ce17443db11b853b6ac5ef3b0f61683db
euichanhwang <pine6971@naver.com>
2023-07-14 21:24:04 +0900

    feat: 테스트를 위한 댓글 데이터를 생성한다 #86

---
+++ backend/src/test/resources/sql/comment.sql
@@ -0,0 +1,3 @@
+INSERT INTO cafe VALUES (1, '2023-07-14', '2023-07-14', '12-13', '월드컵북로6길', '서울', '서울 마포구', '연남 노티드입니다.', '100.12345', 'HONGDAE', '35.12312', '연남 노티드', '05012341234');
+INSERT INTO member VALUES (1, '2023-07-14', '2023-07-14', 100, 'test@test.com', 'KAKAO', 'chan', 'a87524626', 'profile', 'refresh_token', 'USER', '2023-07-28');
+INSERT INTO comment VALUES (1, '2023-07-14', '2023-07-14', '테스트 댓글', 1, 1);
