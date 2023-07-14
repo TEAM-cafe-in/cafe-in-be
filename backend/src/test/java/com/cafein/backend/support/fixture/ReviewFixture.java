3da072e72125d0290bf63970c490c7e5cb7db60f
Seph-23 <seongmlyu95@gmail.com>
2023-07-14 17:26:46 +0900

    test: Review Service Layer Test를 위한 Fixture를 추가한다 #84

--- backend/src/test/java/com/cafein/backend/support/fixture/ReviewFixture.java
+++ backend/src/test/java/com/cafein/backend/support/fixture/ReviewFixture.java
@@ -1,13 +1,38 @@
 package com.cafein.backend.support.fixture;
 
-import com.cafein.backend.api.review.dto.ReviewDTO;
+import static com.cafein.backend.api.review.dto.ReviewDTO.*;
+
+import java.util.List;
+
+import com.cafein.backend.domain.review.constant.CafeCongestion;
+import com.cafein.backend.domain.review.entity.Review;
 
 public class ReviewFixture {
 
-	public static final ReviewDTO.ReviewResponse REVIEW_RESPONSE = createReviewResponse();
+	public static final Review REVIEW = createReview();
+	public static final ReviewRequest REVIEW_REQUEST = createReviewRequest();
+	public static final List<Long> CAFE_IDS_OF_RECENT_REVIEWS = List.of(1L, 2L, 3L);
 
-	private static ReviewDTO.ReviewResponse createReviewResponse() {
-		return ReviewDTO.ReviewResponse.builder()
+	public static final ReviewResponse REVIEW_RESPONSE = createReviewResponse();
+
+	private static Review createReview() {
+		return Review.builder()
+			.cafeCongestion(CafeCongestion.HIGH)
+			.isClean(true)
+			.hasPlug(true)
+			.build();
+	}
+
+	private static ReviewRequest createReviewRequest() {
+		return ReviewRequest.builder()
+			.cafeCongestion("3")
+			.isClean("true")
+			.hasPlug("true")
+			.build();
+	}
+
+		private static ReviewResponse createReviewResponse() {
+		return ReviewResponse.builder()
 			.coffeeBean(102)
 			.build();
 	}
