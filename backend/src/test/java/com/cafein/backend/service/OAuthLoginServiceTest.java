529da73cfb7cdaf33e15ab2e2ea0898bff5248fb
Seph-23 <seongmlyu95@gmail.com>
2023-07-25 11:28:31 +0900

    refactor: MemberType Util Test가 따로 존재하므로 제거한다 #92

--- backend/src/test/java/com/cafein/backend/service/OAuthLoginServiceTest.java
+++ backend/src/test/java/com/cafein/backend/service/OAuthLoginServiceTest.java
@@ -9,7 +9,10 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import com.cafein.backend.api.login.dto.OAuthLoginDTO;
@@ -21,6 +24,7 @@ import com.cafein.backend.external.oauth.kakao.service.KakaoLoginApiServiceImpl;
 import com.cafein.backend.support.utils.ServiceTest;
 
 @ServiceTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class OAuthLoginServiceTest {
 
 	@Autowired
@@ -34,20 +38,6 @@ class OAuthLoginServiceTest {
 
 	@MockBean
 	private GoogleLoginApiServiceImpl googleLoginApiServiceImpl;
-
-	@Test
-	void 카카오_로그인을_시도하면_true를_반환한다() {
-		given(kakaoLoginApiServiceImpl.getUserInfo(ACCESS_TOKEN)).willReturn(KAKAO_OAUTH_ATTRIBUTES);
-		given(memberService.registerMember(any())).willReturn(MEMBER);
-		assertThatNoException().isThrownBy(() -> oAuthLoginService.oauthLogin(ACCESS_TOKEN, MemberType.KAKAO));
-	}
-
-	@Test
-	void 구글_로그인을_시도하면_true를_반환한다() {
-		given(googleLoginApiServiceImpl.getUserInfo(ACCESS_TOKEN)).willReturn(GOOGLE_OAUTH_ATTRIBUTES);
-		given(memberService.registerMember(any())).willReturn(MEMBER);
-		assertThatNoException().isThrownBy(() -> oAuthLoginService.oauthLogin(ACCESS_TOKEN, MemberType.GOOGLE));
-	}
 
 	@Test
 	void 신규로_로그인을하면_회원을_등록하고_토큰을_생성한다() {
