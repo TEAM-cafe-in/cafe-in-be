27bd936cead2cd1f938828710f186cc4ef6776bb
Seph-23 <seongmlyu95@gmail.com>
2023-07-25 15:45:11 +0900

    test: 통합 테스트용 데이터를 insert하는 쿼리 doc을 생성한다 #92
    Co-authored-by: euichanhwang <pine6971@naver.com>

---
+++ backend/src/test/resources/sql/integration.sql
@@ -0,0 +1,15 @@
+INSERT INTO member VALUES (1, '2023-07-14', '2023-07-14', 100, 'test1@test.com', 'KAKAO', 'chan', null, 'profile', 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJtZW1iZXJJZCI6IjEiLCJyb2xlIjoiVVNFUiJ9.MtLTD4fHhcVtNllqCscOyOoIz1e5SegUi5L17pvVIpKm7-xfcBK_gZDFCTNlPcbMZomJ6dYxlSIIpidxhl7nZA', 'USER', '2023-07-28');
+INSERT INTO member VALUES (2, '2023-07-14', '2023-07-14', 100, 'test2@test.com', 'KAKAO', 'lyu', null, 'profile', 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJtZW1iZXJJZCI6IjIiLCJyb2xlIjoiVVNFUiJ9.STwszgdDFg-SRjTzBd-wp9aT8lmBZ0ORERs7CmgQlvWkfWcpVau6w4osNC1Xc2SuVkJIvmG3YvIMW9OL02cVNg', 'USER', '2023-07-28');
+INSERT INTO member VALUES (3, '2023-07-14', '2023-07-14', 100, 'test3@test.com', 'KAKAO', 'kim', null, 'profile', 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJtZW1iZXJJZCI6IjMiLCJyb2xlIjoiVVNFUiJ9.V5_pq7ZT65gu7o3oaSHCGguQsHCPTzk3zWWrpVHVy_JsfYduAz7dkzZFhQsG69WzTLkl67tatTfeFykgoNSo0w', 'USER', '2023-07-28');
+
+INSERT INTO cafe VALUES (1, '2023-07-14', '2023-07-14', '12-13', '월드컵북로6길', '서울', '서울시 마포구', '연남 노티드입니다.', '100.12345', 'HONGDAE', '35.12312', '연남 노티드', '05012341234');
+INSERT INTO cafe VALUES (2, '2023-07-14', '2023-07-14', '5 1층', '상원1길', '서울', '서울시 성동구', '브래디포스트 성수점 입니다.', '101.12345', 'SEONGSU', '36.12312', '브레디포스트 성수', '05032141234');
+INSERT INTO cafe VALUES (3, '2023-07-14', '2023-07-14', '10-11', '상원2길', '서울', '서울시 성동구', '블루보틀 성수 입니다.', '101.12345', 'SEONGSU', '36.12312', '블루보틀 성수', '05032141234');
+
+INSERT INTO comment VALUES (1, '2023-07-14', '2023-07-14', '테스트 댓글1', 1, 1);
+INSERT INTO comment VALUES (2, '2023-07-14', '2023-07-14', '테스트 댓글2', 1, 1);
+INSERT INTO comment VALUES (3, '2023-07-14', '2023-07-14', '테스트 댓글3', 2, 2);
+
+INSERT INTO review VALUES (1, '2023-07-14', '2023-07-14', 'HIGH', 1, 1, 1, 1);
+INSERT INTO review VALUES (2, '2023-07-14', '2023-07-14', 'MEDIUM', 1, 1, 2, 1);
+INSERT INTO review VALUES (3, '2023-07-14', '2023-07-14', 'HIGH', 1, 1, 3, 1);
