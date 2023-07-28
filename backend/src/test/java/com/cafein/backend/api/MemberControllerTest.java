dde2d471bb69d29a071e8dea8ecc696518d660b4
euichanhwang <pine6971@naver.com>
2023-07-28 11:11:51 +0900

    test: 회원의 커피콩을 조회하는 API의 컨트롤러 테스트를 작성한다 #96 Co-authored-by: Seph-23 <seongmlyu95@gmail.com>

--- backend/src/test/java/com/cafein/backend/api/MemberControllerTest.java
+++ backend/src/test/java/com/cafein/backend/api/MemberControllerTest.java
@@ -1,5 +1,6 @@
 package com.cafein.backend.api;
 
+import static com.cafein.backend.support.fixture.LoginFixture.*;
 import static com.cafein.backend.support.fixture.MemberFixture.*;
 import static org.mockito.BDDMockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -15,7 +16,6 @@ import com.cafein.backend.api.member.dto.MyPageDTO;
 import com.cafein.backend.api.member.service.MemberInfoService;
 import com.cafein.backend.api.member.service.MyPageService;
 import com.cafein.backend.domain.member.service.MemberService;
-import com.cafein.backend.support.fixture.LoginFixture;
 
 class MemberControllerTest extends ControllerTestSupporter {
 
@@ -60,11 +60,23 @@ class MemberControllerTest extends ControllerTestSupporter {
 			.perform(patch("/api/member/name")
 				.content("{\"name\": \"손흥민\"}")
 				.contentType(MediaType.APPLICATION_JSON_VALUE)
-				.header(HttpHeaders.AUTHORIZATION, LoginFixture.AUTHORIZATION_HEADER_ACCESS)
+				.header(HttpHeaders.AUTHORIZATION, AUTHORIZATION_HEADER_ACCESS)
 			)
 			.andExpect(status().isOk())
 			.andExpect(jsonPath("$").value("Name change successful!"));
 
 		then(memberService).should(times(1)).updateMemberName(anyLong(), eq("손흥민"));
+	}
+
+	@Test
+	void 회원의_커피콩을_조회한다() throws Exception {
+		given(memberService.findMemberByMemberId(anyLong())).willReturn(MEMBER);
+
+		mockMvc(new MemberController(memberService, memberInfoService, myPageService))
+			.perform(get("/api/member/coffeebean")
+				.header(HttpHeaders.AUTHORIZATION, AUTHORIZATION_HEADER_ACCESS)
+			)
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$").value(100L));
 	}
 }
