0db4f6640bc4949d1dfb1f2723827b3c955d3cfd
euichanhwang <pine6971@naver.com>
2023-07-28 14:42:53 +0900

    refactor: 여러 검증이 필요한 경우 `assertAll()`을 사용한다
    Co-authored-by: Seph-23 <seongmlyu95@gmail.com>

--- backend/src/test/java/com/cafein/backend/integration/ReviewIntegrationTest.java
+++ backend/src/test/java/com/cafein/backend/integration/ReviewIntegrationTest.java
@@ -2,8 +2,8 @@ package com.cafein.backend.integration;
 
 import static com.cafein.backend.support.fixture.ReviewFixture.*;
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 
@@ -16,7 +16,7 @@ class ReviewIntegrationTest extends IntegrationSupporter {
 	void 카페에_대한_리뷰를_등록한다() {
 		final var response = post("/api/cafe/4/review", generateAccessHeader(access_token), REVIEW_REQUEST);
 
-		Assertions.assertAll(
+		assertAll(
 			() -> assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value()),
 			() -> assertThat(response.body().jsonPath().getString("reviewId")).isNotNull(),
 			() -> assertThat(memberService.findMemberByMemberId(1L).getCoffeeBean()).isEqualTo(102)
