3405894d796a4e88ef645f573d1ece348b22383e
euichanhwang <pine6971@naver.com>
2023-08-06 15:05:35 +0900

    feat: 모든 API 요청 시 Authorization Header가 필요하므로 스웨거 문서에 전역 설정을 등록한다 #106

--- backend/src/main/java/com/cafein/backend/global/config/SwaggerConfig.java
+++ backend/src/main/java/com/cafein/backend/global/config/SwaggerConfig.java
@@ -9,9 +9,12 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.RequestParameterBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.ParameterType;
+import springfox.documentation.service.RequestParameter;
 import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
@@ -29,7 +32,19 @@ public class SwaggerConfig {
 			.build()
 			.apiInfo(apiInfo())			//API 문서에 대한 정보 추가
 			.securityContexts(Arrays.asList(securityContext()))
-			.securitySchemes(Arrays.asList(apikey()));
+			.securitySchemes(Arrays.asList(apikey()))
+			.globalRequestParameters(globalParameters());
+	}
+
+	private List<RequestParameter> globalParameters() {
+		return Arrays.asList(
+			new RequestParameterBuilder()
+				.in(ParameterType.HEADER)
+				.name("Authorization")
+				.description("HTTP Authorization Header")
+				.required(Boolean.TRUE)
+				.build()
+		);
 	}
 
 	private ApiInfo apiInfo() {
