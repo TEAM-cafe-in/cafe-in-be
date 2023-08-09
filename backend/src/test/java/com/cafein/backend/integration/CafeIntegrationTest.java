8adb8ddcbe862edfa8e6ee7a3351d7ec1b29c091
euichanhwang <pine6971@naver.com>
2023-08-09 15:46:14 +0900

    test: 리뷰가 없는 카페의 혼잡도 조회 요청 시 커피콩을 차감하지 않고 예외를 발생시킨다 #106

--- backend/src/test/java/com/cafein/backend/integration/CafeIntegrationTest.java
+++ backend/src/test/java/com/cafein/backend/integration/CafeIntegrationTest.java
@@ -55,4 +55,23 @@ class CafeIntegrationTest extends IntegrationSupporter {
 			() -> assertThat(response2.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value())
 		);
 	}
+
+	@Test
+	void 리뷰가_없는_카페는_리뷰의_존재_상태를_false로_반환한다() {
+		final var response = get("/api/cafe/4", generateAccessHeader(access_token));
+
+		assertThat(response.jsonPath().getString("cafeInfo.hasReviewed")).isEqualTo("false");
+	}
+
+	@Test
+	void 리뷰가_없는_카페의_혼잡도_조회_요청은_커피콩을_차감하지_않고_예외를_반환한다() {
+		final var response = post("/api/cafe/4", generateAccessHeader(access_token));
+
+		Assertions.assertAll(
+			() -> assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value()),
+			() -> assertThat(memberService.findMemberByMemberId(1L).getCoffeeBean()).isEqualTo(100),
+			() -> assertThat(response.jsonPath().getString("errorMessage"))
+				.isEqualTo("해당 카페에는 리뷰가 존재하지 않아 혼잡도를 확인할 수 없습니다.")
+		);
+	}
 }
